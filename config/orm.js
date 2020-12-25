4e69bebf7ef86fa78f5cf269e768411686d4799b
masakaede <69030790+masakaede@users.noreply.github.com>
2020-12-25 12:11:21 +1100

    initial upload of orm.js burgers_controller.js schema.sql seeds.sql

---
+++ config/orm.js
@@ -0,0 +1,11 @@
+var connection = require("./connection.js");
+
+function preintQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheess Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Beef Burger");
+
+SELECT * FROM burgers;
