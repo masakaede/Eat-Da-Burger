1b832aaaa99094f2295a433747a03808bbc39184
masakaede <69030790+masakaede@users.noreply.github.com>
2020-12-25 15:49:54 +1100

    updated server.js

--- server.js
+++ server.js
@@ -4,3 +4,20 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/bugers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
