49264bbe7654eaea8107445c0866c9b2b877a827
masakaede <69030790+masakaede@users.noreply.github.com>
2021-02-16 23:19:17 +1100

    test connection

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,17 @@ var mysql = require("mysql");
 
 var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "Masakaede19&&",
-        database: "burgers_db"
-    });
-}
+// if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: "localhost",
+//         port: 3306,
+//         user: "root",
+//         password: "Masakaede19&&",
+//         database: "burgers_db"
+//     });
+// }
 // Make connection.
 connection.connect(function (err) {
     if (err) {
